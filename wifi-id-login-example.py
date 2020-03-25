4d1c49810955328f23ed1ba0c78bc90ec4491ef3
medreza <31310263+medreza@users.noreply.github.com>
2020-03-25 15:59:42 +0700

    Simplify example

--- wifi-id-login-example.py
+++ wifi-id-login-example.py
@@ -2,19 +2,22 @@ import wifiid_login as wifi
 
 def main():
     # Your @wifi.id username
-    username_wifi_id = '<username>'
+    username = '<your username>'
     
     # Your @wifi.id password
-    password_wifi_id = '<password>'
+    password = '<your password>'
     
-    # IP address of your connected device/interface to @wifi.id AP
-    ip_client_wifi_id = '<your IP address>'
+    # IP address of your connected device/interface to @wifi.id access point
+    client_ip = '<your IP address>'
     
     # Your @wifi.id login page link. It usually starts with https://welcome2.wifi.id/login/...
     login_page_url = '<https://welcome2.wifi.id/login/...>'
 
+    # Get login parameters from login page URL
     gw_id, mac, wlan, redir = wifi.get_login_params_from_url(login_page_url)
-    got_response, msg = wifi.login(username_wifi_id, password_wifi_id, ip_client_wifi_id, gw_id, mac, wlan, redir)
+
+    # Login to @wifi.id
+    got_response, msg = wifi.login(username, password, client_ip, gw_id, mac, wlan, redir)
 
     print(got_response)
     print(msg)
